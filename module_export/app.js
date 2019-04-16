380f7222d1bd5f8737d3951a4fdaa7f450ffe046
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-16 15:01:17 +0530

    Update app.js

--- module_export/app.js
+++ module_export/app.js
@@ -1,3 +1,5 @@
-var counter = require('./count');
+//In this program we will import a file(count.js) using the inbuilt function 'require()' without ''.
+
+const counter = require('./count');
 
 console.log(counter(['Ankit','Gurjar','Choudhary']));
