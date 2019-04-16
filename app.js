16a9931a2ddf415e14309da038d57490364df08b
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-16 14:52:07 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+//In this program we will import a file(count.js) using the inbuilt function 'require()' without ''.
+
+const counter = require('./count');
+
+console.log(counter(['Ankit','Gurjar','Choudhary']));
