f1cdfa018ab17e168885ac030354c5304e376d54
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-16 15:01:54 +0530

    Update count.js

--- module_export/count.js
+++ module_export/count.js
@@ -1,6 +1,7 @@
+//In this program we are focusing on how to use a file in an another file (app.js)
+
 var count = (arr) =>{
-  return("There are "+arr.length+" elements in the given array");
-};
+  return("There are "+arr.length+" elements in the given array.");
+}
 
-
-module.exports = count;
+module.exports = count; //In order to use this function in some other file we have to first export it using "module.exports".
