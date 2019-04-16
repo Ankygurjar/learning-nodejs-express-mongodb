b1ce7dbba3a7a50682607dab1f86107759d63a7e
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-16 15:02:15 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-//In this program we will import a file(count.js) using the inbuilt function 'require()' without ''.
-
-const counter = require('./count');
-
-console.log(counter(['Ankit','Gurjar','Choudhary']));
