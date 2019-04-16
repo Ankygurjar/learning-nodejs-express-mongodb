f4dc0230f784cb66fd3232493f8f9bedc76e86f0
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-16 14:29:13 +0530

    Create function_functioncall.js

---
+++ function_functioncall.js
@@ -0,0 +1,15 @@
+//Normal function call
+
+var myFun = () =>{
+  console.log("This is from function 1");
+}
+
+myFun();
+
+//Function Call 
+
+var func2 = (val) =>{
+  console.log(val);
+}
+
+func2("This is a function call");
