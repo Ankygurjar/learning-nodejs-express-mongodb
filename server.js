85ec175c8dd73c7cb50884eef66f14d20e66c54f
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-17 15:00:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var http = require('http');
+//In this program we are building a server in our local machine
 
-var server = http.createServer((req,res)=>{
+var http = require('http');//We are importing the Node Js core module 'http' in variable http
+
+var server = http.createServer((req,res)=>{//createServer function is used to create the server
   res.writeHead(200, 'text-plain');
   res.end("Welcome to Node Js");
 });
 
-server.listen(3000, ()=>{
+server.listen(3000, ()=>{//listen function is used to tell about the location of our server, in this case the location is localhost:3000
   console.log("Server has been started...");
 });
