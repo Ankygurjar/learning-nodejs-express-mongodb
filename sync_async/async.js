2978832e9a8bd3ac5ec41ba4f73faee08c3d6ba9
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-17 15:42:52 +0530

    Update async.js

--- sync_async/async.js
+++ sync_async/async.js
@@ -1,10 +1,12 @@
-var fs = require('fs');
+//In this program we will fetch the data in file 'data' asynchronously using calback Function
 
-fs.readFile('data',(err, data)=>{
+var fs = require('fs');// Here we are exporting the core modul 'File Stream'
+
+fs.readFile('data',(err, data)=>{// Here we are reading the file 'data' using readFile() method and also calling a callback function.
   if(err){
     console.log(err);
   }else{
-    console.log(data.toString());
+    console.log(data.toString());//We need to use toString() function in order to acess the file properly.
   }
 });
 
