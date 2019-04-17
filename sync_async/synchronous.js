2d34fa22a5051f46a8e0a7ddceefa22391de65fa
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-17 15:45:19 +0530

    Update synchronous.js

--- sync_async/synchronous.js
+++ sync_async/synchronous.js
@@ -1,6 +1,8 @@
-var fs = require('fs');
+//In this program we are accessing the data of the file synchronously
 
-var data = fs.readFileSync('data');
+var fs = require('fs');//Here we are exporting the core modyle File Stream
 
-console.log(data.toString());
+var data = fs.readFileSync('data');// Here we are exporting the data in the file and storing in data variable.
+
+console.log(data.toString());// We need to convert the data of the file in order to access the data properly.
 console.log("Program is ended");
