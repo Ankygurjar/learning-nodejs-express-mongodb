a549115a8c7ce47a672c15be06e4fc0ebfddddd7
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2019-04-20 17:23:11 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const app = express();
+/*
+So far we were just building server using node js with http module. That approach works but was quite lenghty.
+To remove such time consuming problem we have a backend framework called Express.js which makes communicating with server
+really easy.
 
-app.get('/',(req,res)=>{
+To instll express type: npm install --save express
+After executing the above command you can see express module in package.json file.
+*/
+
+const express = require('express');// Here we are extracting the 'express' module
+const app = express();// Here we are adding a object to our express module
+
+app.get('/',(req,res)=>{//This is an example of making a simple get request through express
   res.send("Hello World!!");
 });
 
